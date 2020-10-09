51a6e6b54661ac3881211e7fe174838247225258
MiltonDevSis <49815038+MiltonDevSis@users.noreply.github.com>
2020-10-09 11:47:35 -0300

    testando commit.

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,15 +1,34 @@
 package program;
 
 import db.Db;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Connection conn = Db.getConection();
-        Db.closeConection();
+        // Declaração das variáveis
+        Connection conn;
+        Statement stmt;
+        ResultSet rs;
 
+        try {
+            conn = Db.getConection();
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery("select * from department");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + " , " + rs.getString("Name"));
+            }
+        }catch (DbException | SQLException e){
+            e.printStackTrace();
+        }
     }
 }
